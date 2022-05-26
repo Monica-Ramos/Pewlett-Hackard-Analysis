697ccf4b68a8627dd25a80ef9b14ad324d0d8418
Monica-Ramos <101136455+Monica-Ramos@users.noreply.github.com>
2022-05-26 18:45:32 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -64,5 +64,47 @@ FROM mentorship_eligibilty
 GROUP BY title
 ORDER BY COUNT (title) DESC;
 
+--Calculate the budget needed to cover the salaries per position
+SELECT *FROM salaries;
+
+SELECT e.emp_no,
+    e.first_name, 
+    e.last_name,
+    t.title, 
+    t.from_date,
+    t.to_date,
+	s.salary
+INTO retirement_titles_salary
+FROM employees as e
+LEFT JOIN titles as t 
+ON (e.emp_no=t.emp_no)
+LEFT JOIN salaries as s
+ON (e.emp_no=s.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+SELECT * FROM retirement_titles_salary;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name, 
+    last_name,
+	title,
+	salary
+INTO unique_titles_salary
+FROM retirement_titles_salary
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT * FROM unique_titles_salary;
+
+SELECT COUNT (title),title, SUM (salary)
+INTO retiring_titles_salary
+FROM unique_titles_salary
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--aprox budget needed for new employees per position
+SELECT* FROM retiring_titles_salary;
+
 
 
