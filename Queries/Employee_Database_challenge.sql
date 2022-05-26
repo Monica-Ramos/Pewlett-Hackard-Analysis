147c6ba448d556e613c005ae91e0cbe1d773c63b
Monica-Ramos <101136455+Monica-Ramos@users.noreply.github.com>
2022-05-26 18:43:14 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,7 +33,36 @@ GROUP BY title
 ORDER BY COUNT (title) DESC;
 
 SELECT* FROM retiring_titles;
+SELECT SUM(count) FROM retiring_titles;
 
+--DELIVERABLE 2
+--create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+    e.first_name, 
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date ,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no=de.emp_no
+INNER JOIN titles as t
+ON e.emp_no=t.emp_no
+WHERE (de.to_date= '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT *FROM mentorship_eligibilty;
+SELECT COUNT(emp_no) emp_no
+FROM mentorship_eligibilty;
+
+SELECT COUNT(emp_no), title
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY COUNT (title) DESC;
 
 
 
